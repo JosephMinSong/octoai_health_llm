b4a9e56cf5577e8bd25521dc185e1c9fca93cb03
Joseph Song <song.josephmin@gmail.com>
2024-06-29 11:52:33 -0700

    removed health_data_scraper and using ronnes fetch code

--- health_data/health_data_scraper.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-URL = "https://www.who.int/news-room/fact-sheets/detail/mental-disorders"
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(id="PageContent_T0643CD2A003_Col00")
-print(results.prettify())
