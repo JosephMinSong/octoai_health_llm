58fdf934d300aa0f1cfb8a91da0b441ce3287ca7
Joseph Song <song.josephmin@gmail.com>
2024-06-29 12:38:39 -0700

    read_data parsing

--- health_data/read_data.py
+++ health_data/read_data.py
@@ -1,22 +1,28 @@
+import os
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.schema import Document
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
 
 def main():
-    file_text = []
-    header = False
-    try:
-        f = open("parsed_content.txt")
-    except OSError as e:
-        print(e)
-        return
+    files = os.listdir("./health_data_txts")
+    file_texts = []
 
-    for line in f:
+    for file in files:
+        with open(f"./health_data_txts/{file}") as f:
+            file_text = f.read()
         text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
-            chunk_size=1024, chunk_overlap=64
+            chunk_size=512, chunk_overlap=64
         )
-        text = text_splitter.split_text(line)
-        print(text)
+        text = text_splitter.split_text(file_text)
+        file_texts.append(Document(page_content=text))
+
+    embeddings = HuggingFaceEmbeddings()
+    vector_store = FAISS.from_documents(
+        file_texts,
+        embedding=embeddings
+    )
 
 
 main()
