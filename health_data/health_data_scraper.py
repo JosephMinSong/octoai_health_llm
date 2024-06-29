34bea8d57626410b01eaafe4fb1af1de8a1c33d3
Joseph Song <song.josephmin@gmail.com>
2024-06-29 11:14:08 -0700

    python beautiful soup

--- health_data/health_data_scraper.py
+++ health_data/health_data_scraper.py
@@ -4,5 +4,6 @@ from bs4 import BeautifulSoup
 URL = "https://www.who.int/news-room/fact-sheets/detail/mental-disorders"
 page = requests.get(URL)
 
-print(page.text)
 soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="PageContent_T0643CD2A003_Col00")
+print(results.prettify())
